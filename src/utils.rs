e445112700378a2f1caa3ec5b01856ad9343b9f4
Tom Herold <tom.herold@scalableminds.com>
2025-06-06 09:58:26 +0200

    specify 16 bit ffmpeg output

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 pub fn vec_u8_to_i16(vector: Vec<u8>) -> Result<Vec<i16>> {
     let vec_i16 = vector
         .chunks_exact(2)
-        .map(|chunk| i16::from_ne_bytes([chunk[0], chunk[1]]))
+        .map(|chunk| i16::from_le_bytes([chunk[0], chunk[1]]))
         .collect();
 
     return Ok(vec_i16);
